f0470103f8d65f228b82368a41e143369de8af85
Kristoffer Karlsson <kristoffer124@gmail.com>
2025-09-01 19:19:05 +0200

    Removed recommendation type

--- Model/Config/Source/RecommendationTypes.php
+++ Model/Config/Source/RecommendationTypes.php
@@ -8,14 +8,12 @@ use Magento\Framework\Data\OptionSourceInterface;
 class RecommendationTypes implements OptionSourceInterface
 {
     public const RECOMMENDATION_TYPE_NOT_SET = 0;
-    public const RECOMMENDATION_TYPE_CROSS_SELL = 1;
     public const RECOMMENDATION_TYPE_CUSTOM = 2;
 
     public function toOptionArray()
     {
         return [
             self::RECOMMENDATION_TYPE_NOT_SET => __('Category'),
-            self::RECOMMENDATION_TYPE_CROSS_SELL => __('Cross sell'),
             self::RECOMMENDATION_TYPE_CUSTOM => __('Custom'),
         ];
     }
