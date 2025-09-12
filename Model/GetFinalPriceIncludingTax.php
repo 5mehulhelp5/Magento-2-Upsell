bda3927b9d317251726aadf9581157428564a197
Kristoffer Karlsson <kristoffer124@gmail.com>
2025-09-12 14:45:14 +0200

    Add price including tax

--- Model/GetFinalPriceIncludingTax.php
+++ Model/GetFinalPriceIncludingTax.php
@@ -5,19 +5,50 @@ namespace Walley\Upsell\Model;
 
 use Magento\Catalog\Api\Data\ProductInterface;
 use Magento\Catalog\Pricing\Price\FinalPrice as FinalPriceCode;
-use Magento\Framework\Pricing\PriceCurrencyInterface;
-use Magento\Store\Model\StoreManagerInterface;
-use Magento\Tax\Pricing\Adjustment as TaxAdjustment;
+use Magento\Catalog\Helper\Data as CatalogHelper;
+use Magento\Tax\Helper\Data as TaxHelper;
 
 class GetFinalPriceIncludingTax
 {
+    /**
+     * @var CatalogHelper
+     */
+    protected $catalogHelper;
+
+    /**
+     * @var TaxHelper
+     */
+    protected $taxHelper;
+
+    public function __construct(
+        CatalogHelper $catalogHelper,
+        TaxHelper $taxHelper
+    ) {
+        $this->catalogHelper = $catalogHelper;
+        $this->taxHelper = $taxHelper;
+    }
+
     public function execute(ProductInterface $product, int $storeId): float
     {
         $product->setStoreId($storeId);
-        $amount = $product->getPriceInfo()
-            ->getPrice(FinalPriceCode::PRICE_CODE)
-            ->getAmount();
 
-        return $amount->getValue();
+        $final = $product->getPriceInfo()
+            ->getPrice(FinalPriceCode::PRICE_CODE)
+            ->getValue();
+
+        $priceIncludesTax = $this->taxHelper->priceIncludesTax($storeId);
+
+        $incl = $this->catalogHelper->getTaxPrice(
+            $product,
+            $final,
+            true,
+            null,
+            null,
+            null,
+            $storeId,
+            $priceIncludesTax
+        );
+
+        return (float)$incl;
     }
 }
